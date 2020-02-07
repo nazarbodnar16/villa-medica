775cf02b402b84f78f7d241ffa65b045e9f33c01
Nazar Bodnar <nazar.bodnar@perfectial.com>
2020-02-07 17:22:57 +0200

    add 2block to main page

--- front-page.php
+++ front-page.php
@@ -202,8 +202,48 @@ get_header(); ?>
 				<svg width="20" height="8" viewBox="0 0 20 8" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<path class="arrow" d="M19.3536 4.35355C19.5488 4.15829 19.5488 3.84171 19.3536 3.64644L16.1716 0.464465C15.9763 0.269203 15.6597 0.269203 15.4645 0.464465C15.2692 0.659727 15.2692 0.976309 15.4645 1.17157L18.2929 4L15.4645 6.82843C15.2692 7.02369 15.2692 7.34027 15.4645 7.53553C15.6597 7.73079 15.9763 7.73079 16.1716 7.53553L19.3536 4.35355ZM4.37114e-08 4.5L19 4.5L19 3.5L-4.37114e-08 3.5L4.37114e-08 4.5Z" fill="#716F74"/>
 				</svg>
-
 			</a>
+		</div>
+	</div>
+	<div class="our-doctors">
+		<div class="container">
+			<h2>Наши профессионалы</h2>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<div class="doctor-card">
+							<a href="">
+								<div class="ava bg-img">
+									<img src="<?php echo get_template_directory_uri(); ?>/images/doctor-1.png" alt="">
+								</div>
+								<div class="doctor-descr">
+									<strong>Олег Дмитриевич Иванов</strong>
+									<p>Доктор наук. ведение патологической беременности, задержка роста плода...</p>
+									<span class="detail-btn">Детальнее</span>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="get-in-touch">
+		<div class="container">
+			<div class="get-in-touch-form">
+				<div class="touch-content">
+					<h2>Запись на Консультацию</h2>
+					<a href="" class="btn-arrow">
+						<svg class="hover-line">
+							<rect class="hover-rect" x="0" y="0" fill="none" width="100%" height="100%"></rect>
+						</svg>
+						<span class="text">Заполнить форму</span>
+						<svg width="20" height="8" viewBox="0 0 20 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path class="arrow" d="M19.3536 4.35355C19.5488 4.15829 19.5488 3.84171 19.3536 3.64644L16.1716 0.464465C15.9763 0.269203 15.6597 0.269203 15.4645 0.464465C15.2692 0.659727 15.2692 0.976309 15.4645 1.17157L18.2929 4L15.4645 6.82843C15.2692 7.02369 15.2692 7.34027 15.4645 7.53553C15.6597 7.73079 15.9763 7.73079 16.1716 7.53553L19.3536 4.35355ZM4.37114e-08 4.5L19 4.5L19 3.5L-4.37114e-08 3.5L4.37114e-08 4.5Z" fill="#716F74"/>
+						</svg>
+					</a>
+				</div>
+			</div>
 		</div>
 	</div>
 	<div class="bottom-slider">
